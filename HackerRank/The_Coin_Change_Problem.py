3d115e409655c539f2707c6840d72464526e8ac5
Prakhar Kumar <prakharkumar506978@gmail.com>
2020-03-10 10:19:33 +0530

    10-03-2020

--- HackerRank/The_Coin_Change_Problem.py
+++ HackerRank/The_Coin_Change_Problem.py
@@ -1,13 +1,23 @@
-count = 0
+# def change(a, m, n):
+#     if n == 0:
+#         return 1
+#     if n < 0:
+#         return 0
+#     if m <= 0 and n >= 1:
+#         return 0
+#     return change(a, m-1, n) + change(a, m, n-a[m-1])
 
 
-def change(a, t, length):
-    global count
-    if t == 0:
-        return 1
-    else:
-        
+def change(a, m, n):
+    table = [0]*(n+1)
+    table[0] = 1
+    for i in range(m):
+        for j in range(a[i], n+1):
+            table[j] += table[j - a[i]]
+    # print(table)
+    return table[n]
 
 
-n, m = map(int, input().split())
+N, M = map(int, input().split())
 arr = list(map(int, input().split()))
+print(change(arr, M, N))
