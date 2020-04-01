70e0272bed1af72b44ea7e54427038b1bfe8271b
unknown <prakharkumar506978@gmail.com>
2020-04-01 10:08:20 +0530

    01-04-2020

---
+++ HackerRank/Data_Structures/Trees/Tree_Preorder_Traversal.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class Tree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, root, data):
+        if not root:
+            return Node(data)
+        
+        if data < root.data:
+            root.left = self.insert(root.left, data)
+        else:
+            root.right = self.insert(root.right, data)
+
+        return root
+
+def preorder_traversal(root):
+    if root:
+        print(root.data)
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
+tree = Tree()
+inp = list(map(int, input().split()))
+for i in inp:
+    tree.root = tree.insert(tree.root, i)
+preorder_traversal(tree.root)
