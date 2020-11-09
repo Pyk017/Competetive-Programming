ccb896a44a19d84cf3d54f6d8ba9610c2dc6d611
unknown <prakharkumar506978@gmail.com>
2020-11-09 08:31:53 +0530

    09-11-2020

---
+++ Interview_Preparation_Questions/Top_Algorithms_Topic_Wise/String_and_Arrays/Reverse_Array_without_affecting_special_Characters.py
@@ -0,0 +1,37 @@
+import string
+
+# def reverse(st):
+#     stack = []
+#     for i in st:
+#         if i in string.ascii_lowercase:
+#             stack.append(i)
+
+#     st = list(st)
+#     for i in range(len(st)):
+#         if st[i] in string.ascii_lowercase:
+#             st[i] = stack.pop()
+
+#     return ''.join(st)
+
+
+def reverse(st):
+    n = len(st)
+    l, r = 0, n - 1
+    st = list(st)
+    while l <= r:
+        if st[l].isalpha():
+            l += 1
+        elif st[r].isalpha():
+            r -= 1
+        else:
+            st[l], st[r] = st[r], st[l]
+            l += 1
+            r -= 1
+
+    return st
+
+
+
+st = input()
+res = reverse(st)
+print(res)
