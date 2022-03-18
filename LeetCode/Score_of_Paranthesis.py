475c6afb3d8d04d05b44ff8302d1df79a37cfe85
Prakhar Kumar Kashyap <prakharkumar506978@gmail.com>
2022-03-18 13:36:17 +0530

    '18-03-2022'

---
+++ LeetCode/Score_of_Paranthesis.py
@@ -0,0 +1,22 @@
+def scoreOfParanthesis(s):
+	stack = []
+	score = 0
+	n = len(s)
+
+	for i in range(n):
+		if s[i] == "(":
+			stack.append(score)
+			score = 0
+		else:
+			score = stack.pop() + max(2 * score, 1)
+
+
+	return score
+
+
+
+
+string = input()
+result = scoreOfParanthesis(string)
+print(result)
+
